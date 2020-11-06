3cf5eab79f3a0c0ae0ebc3464367cf562321960d
Miguel Monroy Lara <migue@MacBook-Pro-de-MacBook.local>
2020-11-06 15:23:57 -0600

    Initial commit

---
+++ monitorServer/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for monitorServer project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitorServer.settings')
+
+application = get_asgi_application()
