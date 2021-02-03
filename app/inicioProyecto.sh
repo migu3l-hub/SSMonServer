aa76e3d8e6d32584d4878030bdbfcb1b6a1af6f8
gguevara.r20 <gguevara.r20@lania.edu.mx>
2021-02-03 13:15:36 -0600

    Se pasa el proyecto a Docker pero aun no es probado

---
+++ app/inicioProyecto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 15
+
+python3 -u manage.py makemigrations
+python3 -u manage.py migrate
+gunicorn --bind :8000 monitorServer.wsgi:application --reload
+#python3 -u manage.py runserver 0.0.0.0:8000
+
--- app/monitorServer/monitorServer/settings.py
+++ app/monitorServer/monitorServer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!c)#_^&inv2m2*ru_l8bk!a2z0=b(_fyww7=^1-e)op5j063zi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
